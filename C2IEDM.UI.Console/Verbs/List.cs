3ce1860a600b18d6394547fbef403472f7abfdea
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-25 20:31:28 +0100

    -

---
+++ C2IEDM.UI.Console/Verbs/List.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace C2IEDM.UI.Console.Verbs;
+
+[Verb("list", HelpText = "List all people.")]
+public sealed class List
+{
+    [Option('e', "entity", Required = true, HelpText = "Entity")]
+    public string Entity { get; set; }
+
+}
