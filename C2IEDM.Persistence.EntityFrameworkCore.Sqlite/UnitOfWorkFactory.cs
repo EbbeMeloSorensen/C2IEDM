49fba92efa727b1e9991e5a454a1d20bb606acb4
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-25 16:47:51 +0100

    -

---
+++ C2IEDM.Persistence.EntityFrameworkCore.Sqlite/UnitOfWorkFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace C2IEDM.Persistence.EntityFrameworkCore.Sqlite;
+
+public class UnitOfWorkFactory : IUnitOfWorkFactory
+{
+    static UnitOfWorkFactory()
+    {
+        using var context = new C2IEDMDbContext();
+        context.Database.EnsureCreated();
+
+        if (context.Locations.Any()) return;
+
+        Seeding.SeedDatabase(context);
+    }
+
+    public IUnitOfWork GenerateUnitOfWork()
+    {
+        return new UnitOfWork(new C2IEDMDbContext());
+    }
+}
